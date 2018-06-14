0610266da7b3bf9567fd68c04613add85ffd21e6
Rodrigo Klaes <rodrigoklaes@gmail.com>
2018-06-14 17:52:00 -0300

    Navbar;Carossel; Import libs

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+		items: 1,
+		loop: true,
+		center: true,
+		margin: 0,
+		lazyLoad:true,
+		dots: true
+    });
+  });
+
