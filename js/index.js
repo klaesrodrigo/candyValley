a6fe96bd76b085317ebc0622c6ca0d897479dcdc
Rodrigo Klaes <rodrigoklaes@gmail.com>
2018-07-24 08:54:01 -0300

    Animação dos números

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         nav: false,
@@ -9,11 +7,23 @@ $(document).ready(function () {
         autoHeight: true
     });
 });
+console.log("Testando: " + $($("#testando")).offset().top)
 
-
-$("a.menu").click(function() {
+$("a.menu").click(function () {
     var elemento = $(this).attr('href');
     $('html, body').animate({
         scrollTop: $(elemento).offset().top
     }, 500);
 });
+
+let bool = true;
+$(document).scroll(() => {
+    if ((window.scrollY > $(".numeros").offset().top - window.innerHeight / 1.25) && bool) {
+        $('.counter1').animateNumber({ number: 455 }, 1200);
+        $('.counter2').animateNumber({ number: 1909100 }, 800);
+        $('.counter3').animateNumber({ number: 3599 }, 1000);
+        bool = false;
+    }
+});
+
+
